5921b6ce13c5f9da0757642ed43128da018914d4
ChanHoe Choi <50437623+cks8483@users.noreply.github.com>
2022-05-31 15:27:57 +0900

    [feat] terraform output 추가

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,22 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+output "vpc_id" {
+  value = aws_vpc.aws-vpc.id
+}
+output "aws_security_group_id" {
+  value =  aws_security_group.load_balancer_security_group.id
+}
+output "aws_subnet_public_id" {
+  value =  aws_subnet.public.*.id
+}
+output "aws_subnet_private_id" {
+  value =  aws_subnet.private.*.id
+}
+output "aws_tg_id" {
+  value = aws_lb_target_group.target_group.id  
+}
+output "aws_tg_id2" {
+  value = aws_lb_target_group.target_group2.id
+}
+
