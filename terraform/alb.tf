0eac38b2ce222edfcf2706d5a3259515e192c04e
ChanHoe Choi <50437623+cks8483@users.noreply.github.com>
2022-06-02 17:08:47 +0900

    [fix] terraform 코드 수정

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -112,7 +112,7 @@ resource "aws_lb_listener" "listener3" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
 }
 resource "aws_lb_target_group" "target_group2" {
